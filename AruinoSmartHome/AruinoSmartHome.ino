6eca507fe163979786d1e7ec44dc0100d890b01d
YiWei <ghostgodgame@gmail.com>
2020-09-28 16:18:50 +0800

    修正復位後警示燈不會熄滅問題, close #1

--- AruinoSmartHome/AruinoSmartHome.ino
+++ AruinoSmartHome/AruinoSmartHome.ino
@@ -3,9 +3,12 @@ void setup()
 pinMode(12, OUTPUT);
 pinMode(11, OUTPUT);
 pinMode(7, OUTPUT);
+pinMode(2, INPUT);
+Serial.begin(9600);
 }
 void loop()
 {
+if(digitalRead(2)==HIGH) {
 digitalWrite(12, HIGH);
 digitalWrite(11, LOW);
 tone(7, 1000, 500);
@@ -14,4 +17,9 @@ digitalWrite(12, LOW);
 digitalWrite(11, HIGH);
 tone(7, 800, 500);
 delay(500);
+}
+else {
+digitalWrite(12, LOW);
+digitalWrite(11, LOW);
+}
 }
