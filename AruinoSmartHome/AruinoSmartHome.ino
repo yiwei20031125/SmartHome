6fc4e00eb1f17342c2a489d18b1477be4ebecbf7
YiWei <ghostgodgame@gmail.com>
2020-09-28 16:30:22 +0800

    依狀態不同送出 alert, normal 訊息給控制器

--- AruinoSmartHome/AruinoSmartHome.ino
+++ AruinoSmartHome/AruinoSmartHome.ino
@@ -9,6 +9,7 @@ Serial.begin(9600);
 void loop()
 {
 if(digitalRead(2)==HIGH) {
+Serial.println("alert");
 digitalWrite(12, HIGH);
 digitalWrite(11, LOW);
 tone(7, 1000, 500);
@@ -19,7 +20,9 @@ tone(7, 800, 500);
 delay(500);
 }
 else {
+Serial.println("normal");
 digitalWrite(12, LOW);
 digitalWrite(11, LOW);
+delay(1000);
 }
 }
